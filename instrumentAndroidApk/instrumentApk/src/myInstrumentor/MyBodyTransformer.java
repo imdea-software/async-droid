f75282135dfaa7a0953f6b4349c72cb27dc6df2f
Burcu Ozkan <burcukulahcioglu@gmail.com>
2014-06-30 17:30:41 +0200

    doInBackground additional return stmt after code block is removed.

--- instrumentAndroidApk/instrumentApk/src/myInstrumentor/MyBodyTransformer.java
+++ instrumentAndroidApk/instrumentApk/src/myInstrumentor/MyBodyTransformer.java
@@ -36,19 +36,24 @@ public class MyBodyTransformer extends BodyTransformer {
 		exitMonitor = schedulerClass.getMethod("void exitMonitor()");
 
 		if(b.getMethod().getDeclaringClass().toString().equals("myScheduler.MyScheduler")){
-			System.out.println("Skipping myScheduler.MyScheduler");
+			//System.out.println("Skipping myScheduler.MyScheduler");
 			return;
 		}
 		if(b.getMethod().getDeclaringClass().toString().equals("myScheduler.SchedulerRunnable")){
-			System.out.println("Skipping myScheduler.SchedulerRunnable");
+			//System.out.println("Skipping myScheduler.SchedulerRunnable");
 			return;
 		}
 		if(b.getMethod().getDeclaringClass().toString().equals("myScheduler.PendingThreads")){
-			System.out.println("Skipping myScheduler.PendingThreads");
+			//System.out.println("Skipping myScheduler.PendingThreads");
 			return;
 		}
 		if(b.getMethod().getDeclaringClass().toString().equals("myScheduler.ThreadData")){
-			System.out.println("Skipping myScheduler.ThreadData");
+			//System.out.println("Skipping myScheduler.ThreadData");
+			return;
+		}
+		
+		if(b.getMethod().getDeclaringClass().toString().startsWith("android.support")){
+			//System.out.println("Skipping android.support methods");
 			return;
 		}
 		
@@ -68,9 +73,8 @@ public class MyBodyTransformer extends BodyTransformer {
 							StaticInvokeExpr initiateExpr = Jimple.v().newStaticInvokeExpr(initiateScheduler.makeRef());
 							Unit initiateStmt = Jimple.v().newInvokeStmt(initiateExpr);
 							units.insertAfter(initiateStmt, stmt);
-							System.out.println("Initiate Scheduler stmt added..");
+							System.out.println("===========Initiate Scheduler stmt added..");
 						}
-						//System.out.println(stmt.getInvokeExpr().getMethod().getSignature().toString());
 					}
 				});
 				u = iter.next();
@@ -78,10 +82,12 @@ public class MyBodyTransformer extends BodyTransformer {
 			return;
 		}
 		
+		//TODO: If parent is pure Thread, then detect loop and instrument beginning and the end of the loop
 		// runnables posted to a looper thread
 		// by post, postAtFrontOfQueue, postAtTime, postDelayed 
 		// or sent to UI thread by runOnUIThread
 		if (b.getMethod().getName().equals("run")) {
+			System.out.println("===========Instrumenting run..");
 			instrumentMethod(units, u, iter);
 			return;
 		}
@@ -89,31 +95,83 @@ public class MyBodyTransformer extends BodyTransformer {
 		// handling of messages sent to a looper thread
 		// by sendMessage, sendMessageAtFrontOfQueue, sendMessageAtTime, sendMessageDelayed
 		if (b.getMethod().getName().equals("handleMessage")) {
+			System.out.println("===========Instrumenting handleMessage..");
 			instrumentMethod(units, u, iter);
 			return;
 		}
 
 		if (b.getMethod().getName().equals("onPreExecute")) {
+			System.out.println("===========Instrumenting onPreExecute..");
 			instrumentMethod(units, u, iter);
 			return;
 		}
 
 		if (b.getMethod().getName().equals("onPostExecute")) {
+			System.out.println("===========Instrumenting onPostExecute..");
 			instrumentMethod(units, u, iter);
 			return;
 		}
 
 		if (b.getMethod().getName().equals("onProgressUpdate")) {
+			System.out.println("===========Instrumenting donProgressUpdate..");
 			instrumentMethod(units, u, iter);
 			return;
 		}
 
 		if (b.getMethod().getName().equals("doInBackground")) {
-			instrumentMethod(units, u, iter);
+			System.out.println("===========Instrumenting doInBackground..");
+			StaticInvokeExpr waitExpr = Jimple.v().newStaticInvokeExpr(waitMyTurn.makeRef());
+			Unit waitStmt = Jimple.v().newInvokeStmt(waitExpr);
+			units.insertAfter(waitStmt, u);
+			System.out.println("Wait for CPU stmt added..");
+
+			while (iter.hasNext()) {
+
+				u.apply(new AbstractStmtSwitch() {
+
+					public void caseReturnVoidStmt(ReturnVoidStmt stmt) {
+						StaticInvokeExpr notifyExpr = Jimple.v().newStaticInvokeExpr(notifyScheduler.makeRef());
+						Unit notifyStmt = Jimple.v().newInvokeStmt(notifyExpr);
+						units.insertBefore(notifyStmt, stmt);
+						System.out.println("Release CPU stmt added..");
+					}
+
+					public void caseReturnStmt(ReturnStmt stmt) {
+						StaticInvokeExpr notifyExpr = Jimple.v().newStaticInvokeExpr(notifyScheduler.makeRef());
+						Unit notifyStmt = Jimple.v().newInvokeStmt(notifyExpr);
+						units.insertBefore(notifyStmt, stmt);
+						System.out.println("Release CPU stmt added..");
+					}
+
+					public void caseRetStmt(RetStmt stmt) {
+						StaticInvokeExpr notifyExpr = Jimple.v().newStaticInvokeExpr(notifyScheduler.makeRef());
+						Unit notifyStmt = Jimple.v().newInvokeStmt(notifyExpr);
+						units.insertBefore(notifyStmt, stmt);
+						System.out.println("Release CPU stmt added..");
+					}
+
+					public void caseEnterMonitorStmt(EnterMonitorStmt stmt){
+						StaticInvokeExpr enterMonitorExpr = Jimple.v().newStaticInvokeExpr(enterMonitor.makeRef());
+						Unit enterMonitorStmt = Jimple.v().newInvokeStmt(enterMonitorExpr);
+						units.insertAfter(enterMonitorStmt, stmt);
+						System.out.println("Enter monitor stmt added..");
+					}
+
+					public void caseExitMonitorStmt(ExitMonitorStmt stmt){
+						StaticInvokeExpr exitMonitorExpr = Jimple.v().newStaticInvokeExpr(exitMonitor.makeRef());
+						Unit exitMonitorStmt = Jimple.v().newInvokeStmt(exitMonitorExpr);
+						units.insertAfter(exitMonitorStmt, stmt);
+						System.out.println("Exit monitor stmt added..");
+					}
+				});
+				u = iter.next();
+			}
 			return;
 		}
 
 	}
+	
+	
 	
 	public void instrumentMethod(final PatchingChain<Unit> units, Unit u, Iterator<Unit> iter){
 		StaticInvokeExpr waitExpr = Jimple.v().newStaticInvokeExpr(waitMyTurn.makeRef());
@@ -168,6 +226,7 @@ public class MyBodyTransformer extends BodyTransformer {
 		units.insertBefore(notifyStmt, u);
 		System.out.println("Release CPU stmt added before the last unit..");
 	}
+
 
 
 }
