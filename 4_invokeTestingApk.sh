8d0ef4ad1d21227a5741d20c0ff44b2f91e4857a
Burcu Ozkan <burcukulahcioglu@gmail.com>
2014-07-07 11:28:49 +0200

    Improved logging

--- 4_invokeTestingApk.sh
+++ 4_invokeTestingApk.sh
@@ -4,6 +4,8 @@ PATH_TO_SIGNED_APK="${PWD}/example/signed/HelloWorldApp.apk"
 #PATH_TO_SIGNED_APK= # full path to your instrumented and signed application file
 PATH_TO_SIGNED_TESTER="${PWD}/example/signed/TestApk-release-signed.apk"
 #PATH_TO_SIGNED_APK= # full path to your signed tester application file
+APK_PACKAGE_NAME="my.example.HelloWorld"
+#APK_PACKAGE_NAME= # package name of your app
 
 # Install the app to be tested 
 # Comment out the following line if your app is already installed
@@ -11,15 +13,16 @@ monkeyrunner ${PWD}/invokeTestingApk/scripts/installScript.py ${PATH_TO_SIGNED_A
 monkeyrunner ${PWD}/invokeTestingApk/scripts/installScript.py ${PATH_TO_SIGNED_TESTER}
 
 mkdir logcatOutputs
-cd logcatOutputs
-touch "MyScheduler.log"
-cd ..
+adb logcat ActivityManager:I *:S --line-buffered > "${PWD}/logcatOutputs/AllActivities.log" &
 adb logcat -s "MyScheduler" --line-buffered > "${PWD}/logcatOutputs/MyScheduler.log" &
+adb logcat -s *:E --line-buffered > "${PWD}/logcatOutputs/AllErrors.log" &
 
 
 # Invoke testing
 adb shell am instrument -w my.example.test/android.test.InstrumentationTestRunner
 
 
-#TODO: Kill the child process
+# Kill the child processes
+trap "kill 0" SIGINT SIGTERM EXIT
+
 exit
