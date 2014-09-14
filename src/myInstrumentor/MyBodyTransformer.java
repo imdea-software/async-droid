95d8dda77b90169cb1eb339c80296ab0dfac153f
Michael Emmi <michael.emmi@gmail.com>
2014-09-14 12:26:53 +0200

    Cleaned up instrumentation code.

--- src/myInstrumentor/MyBodyTransformer.java
+++ src/myInstrumentor/MyBodyTransformer.java
@@ -24,15 +24,17 @@ import soot.jimple.ThisRef;
 import soot.jimple.StaticInvokeExpr;
 import soot.jimple.VirtualInvokeExpr;
 
-
 public class MyBodyTransformer extends BodyTransformer {
 
     static SootClass schedulerClass;
     static SootMethod initiateScheduler, waitMyTurn, notifyScheduler, enterMonitor, exitMonitor;
 
-    @Override
-    protected void internalTransform(final Body b, String phaseName,
-            @SuppressWarnings("rawtypes") Map options) {
+    static SootClass activityClass;
+    static SootMethod getApplicationContextMethod;
+
+    void init() {
+        if (schedulerClass != null)
+            return;
 
         schedulerClass = Scene.v().getSootClass("myScheduler.MyScheduler");
         initiateScheduler = schedulerClass.getMethod("void initiateScheduler(android.content.Context)");
@@ -41,188 +43,135 @@ public class MyBodyTransformer extends BodyTransformer {
         enterMonitor = schedulerClass.getMethod("void enterMonitor()");
         exitMonitor = schedulerClass.getMethod("void exitMonitor()");
 
-        if(b.getMethod().getDeclaringClass().toString().equals("myScheduler.MyScheduler")){
-            //System.out.println("Skipping myScheduler.MyScheduler");
-            return;
-        }
-        if(b.getMethod().getDeclaringClass().toString().equals("myScheduler.SchedulerRunnable")){
-            //System.out.println("Skipping myScheduler.SchedulerRunnable");
-            return;
-        }
-        if(b.getMethod().getDeclaringClass().toString().equals("myScheduler.DelayServiceConHandler")){
-            //System.out.println("Skipping myScheduler.DelayServiceConHandler");
-            return;
-        }
-        if(b.getMethod().getDeclaringClass().toString().equals("myScheduler.DelayServiceConHandler$IncomingMessageHandler")){
-            //System.out.println("Skipping myScheduler.DelayServiceConHandler$IncomingMessageHandler");
-            return;
-        }
-        if(b.getMethod().getDeclaringClass().toString().equals("myScheduler.DelayGenerator")){
-            //System.out.println("Skipping myScheduler.DelayGenerator");
-            return;
-        }
-        if(b.getMethod().getDeclaringClass().toString().equals("myScheduler.PendingThreads")){
-            //System.out.println("Skipping myScheduler.PendingThreads");
-            return;
-        }
-        if(b.getMethod().getDeclaringClass().toString().equals("myScheduler.ThreadData")){
-            //System.out.println("Skipping myScheduler.ThreadData");
-            return;
-        }
+        activityClass = Scene.v().getSootClass("android.app.Activity");
+        getApplicationContextMethod = Scene.v().getMethod("<android.content.ContextWrapper: android.content.Context getApplicationContext()>");
+    }
+
+    @Override
+    protected void internalTransform(final Body b, String phaseName,
+            @SuppressWarnings("rawtypes") Map options) {
+
+        init();
+
+        String className = b.getMethod().getDeclaringClass().toString();
+        String methodName = b.getMethod().getName();
+
+        if (className.startsWith("myScheduler.")) {
+
+        } else if (className.startsWith("android.support")) {
+
+        } else if (methodName.equals("onCreate")) {
+            instrumentOnCreateMethod(b);
         
-        if(b.getMethod().getDeclaringClass().toString().startsWith("android.support")){
-            //System.out.println("Skipping android.support methods");
-            return;
+        // TODO: If parent is pure Thread, then detect loop and instrument
+        // beginning and the end of the loop runnables posted to a looper
+        // thread by post, postAtFrontOfQueue, postAtTime, postDelayed
+        // or sent to UI thread by runOnUIThread
+
+        } else if (methodName.equals("run") ||
+                   methodName.equals("handleMessage") ||
+                   methodName.equals("onPreExecute") ||
+                   methodName.equals("onPostExecute") ||
+                   methodName.equals("onProgressUpdate") ||
+                   methodName.equals("doInBackground")) {
+
+            System.out.println("===========Instrumenting " + methodName + "..");
+            instrumentMethod(b);
+
+        } else if (b.getMethod().getDeclaringClass().getSuperclass().toString().equals("android.app.Activity") &&
+                !activityClass.declaresMethod(b.getMethod().getNumberedSubSignature()) ){
+
+            // if a method in an Activity class is defined by user, synchronize 
+            // it with the scheduler: (so that UI gives change to other threads 
+            // when a UI event is received)
+            System.out.println("===========Instrumenting a user defined method: " + b.getMethod());
+            instrumentMethod(b);
         }
-        
+    }
+
+    void instrumentOnCreateMethod(final Body b) {
         final PatchingChain<Unit> units = b.getUnits();
         Iterator<Unit> iter = units.snapshotIterator();
 
-        if (b.getMethod().getName().equals("onCreate")) {
+        while (iter.hasNext()) {
 
-            while (iter.hasNext()) {
+            iter.next().apply(new AbstractStmtSwitch() {
 
-                Unit u = iter.next();
-                u.apply(new AbstractStmtSwitch() {
+                public void caseInvokeStmt(InvokeStmt stmt) {
+                    if (stmt.getInvokeExpr().getMethod().getSignature().equals("<android.app.Activity: void onCreate(android.os.Bundle)>")) {
 
-                    public void caseInvokeStmt(InvokeStmt stmt) {
-                        if(stmt.getInvokeExpr().getMethod().getSignature().equals("<android.app.Activity: void onCreate(android.os.Bundle)>")){
+                        // get application context:
+                        Local context = createLocal(b, "ctx", "android.content.Context");
 
-                            // get activity's this reference
-                            Local thisAct = b.getThisLocal();
-                                                    
-                            // get application context:
-                            Local context = addTmpRef(b, "ctx", "android.content.Context");
-                            SootMethod sm = Scene.v().getMethod("<android.content.ContextWrapper: android.content.Context getApplicationContext()>");    
-                            VirtualInvokeExpr getContextExpr = Jimple.v().newVirtualInvokeExpr(thisAct, sm.makeRef());
-                                                                        
-                            // initiate scheduler with this context
-                            StaticInvokeExpr initiateExpr = Jimple.v().newStaticInvokeExpr(initiateScheduler.makeRef(), context);
-                            Unit initiateStmt = Jimple.v().newInvokeStmt(initiateExpr);
-                            units.insertAfter(initiateStmt, stmt);
-                            units.insertAfter(Jimple.v().newAssignStmt(context, getContextExpr), stmt);  // correct order
-                            System.out.println("===========Initiate Scheduler stmt added..");
-                        }
+                        // initiate scheduler with the application context
+                        units.insertAfter(staticInvocation(initiateScheduler,context), stmt);
+                        units.insertAfter(
+                            Jimple.v().newAssignStmt(context,
+                                Jimple.v().newVirtualInvokeExpr(b.getThisLocal(),
+                                    getApplicationContextMethod.makeRef())),
+                            stmt);
+                        System.out.println("===========Initiate Scheduler stmt added..");
                     }
-                });
-            }
-            return;
+                }
+            });
         }
-        
-        //TODO: If parent is pure Thread, then detect loop and instrument beginning and the end of the loop
-        // runnables posted to a looper thread
-        // by post, postAtFrontOfQueue, postAtTime, postDelayed 
-        // or sent to UI thread by runOnUIThread
-        if (b.getMethod().getName().equals("run")) {
-            System.out.println("===========Instrumenting run..");
-            instrumentMethod(units, iter);
-            return;
-        }
-
-        // handling of messages sent to a looper thread
-        // by sendMessage, sendMessageAtFrontOfQueue, sendMessageAtTime, sendMessageDelayed
-        if (b.getMethod().getName().equals("handleMessage")) {
-            System.out.println("===========Instrumenting handleMessage..");
-            instrumentMethod(units, iter);
-            return;
-        }
-
-        if (b.getMethod().getName().equals("onPreExecute")) {
-            System.out.println("===========Instrumenting onPreExecute..");
-            instrumentMethod(units, iter);
-            return;
-        }
-
-        if (b.getMethod().getName().equals("onPostExecute")) {
-            System.out.println("===========Instrumenting onPostExecute..");
-            instrumentMethod(units, iter);
-            return;
-        }
-
-        if (b.getMethod().getName().equals("onProgressUpdate")) {
-            System.out.println("===========Instrumenting donProgressUpdate..");
-            instrumentMethod(units, iter);
-            return;
-        }
-
-        if (b.getMethod().getName().equals("doInBackground")) {
-            System.out.println("===========Instrumenting doInBackground..");
-            instrumentMethod(units, iter);
-            return;
-        }
-        
-        // if a method in an Activity class is defined by user, synchronize it with the scheduler:
-        // (so that UI gives change to other threads when a UI event is received)    
-        SootClass activityClass = Scene.v().getSootClass("android.app.Activity");
-        if(b.getMethod().getDeclaringClass().getSuperclass().toString().equals("android.app.Activity") && !activityClass.declaresMethod(b.getMethod().getNumberedSubSignature()) ){
-            System.out.println("===========Instrumenting a user defined method: " + b.getMethod());
-            instrumentMethod(units, iter);
-            return;
-        }
-
     }
-    
-    
-    public void instrumentMethod(final PatchingChain<Unit> units, Iterator<Unit> iter){
-        
+
+    void instrumentMethod(final Body b){
+        final PatchingChain<Unit> units = b.getUnits();
+        Iterator<Unit> iter = units.snapshotIterator();
+
         if(!iter.hasNext())
             return;
         
         Unit u = iter.next();
-        StaticInvokeExpr waitExpr = Jimple.v().newStaticInvokeExpr(waitMyTurn.makeRef());
-        Unit waitStmt = Jimple.v().newInvokeStmt(waitExpr);
-        units.insertAfter(waitStmt, u);
+        units.insertAfter(staticInvocation(waitMyTurn), u);
         System.out.println("Wait for CPU stmt added..");
 
         while (iter.hasNext()) {
-
             u = iter.next();
             u.apply(new AbstractStmtSwitch() {
 
                 public void caseReturnVoidStmt(ReturnVoidStmt stmt) {
-                    StaticInvokeExpr notifyExpr = Jimple.v().newStaticInvokeExpr(notifyScheduler.makeRef());
-                    Unit notifyStmt = Jimple.v().newInvokeStmt(notifyExpr);
-                    units.insertBefore(notifyStmt, stmt);
+                    units.insertBefore(staticInvocation(notifyScheduler), stmt);
                     System.out.println("Release CPU stmt added..");
                 }
                 
                 public void caseReturnStmt(ReturnStmt stmt) {
-                    StaticInvokeExpr notifyExpr = Jimple.v().newStaticInvokeExpr(notifyScheduler.makeRef());
-                    Unit notifyStmt = Jimple.v().newInvokeStmt(notifyExpr);
-                    units.insertBefore(notifyStmt, stmt);
+                    units.insertBefore(staticInvocation(notifyScheduler), stmt);
                     System.out.println("Release CPU stmt added..");
                 }
                 
                 public void caseRetStmt(RetStmt stmt) {
-                    StaticInvokeExpr notifyExpr = Jimple.v().newStaticInvokeExpr(notifyScheduler.makeRef());
-                    Unit notifyStmt = Jimple.v().newInvokeStmt(notifyExpr);
-                    units.insertBefore(notifyStmt, stmt);
+                    units.insertBefore(staticInvocation(notifyScheduler), stmt);
                     System.out.println("Release CPU stmt added..");
                 }
                 
                 public void caseEnterMonitorStmt(EnterMonitorStmt stmt){
-                    StaticInvokeExpr enterMonitorExpr = Jimple.v().newStaticInvokeExpr(enterMonitor.makeRef());
-                    Unit enterMonitorStmt = Jimple.v().newInvokeStmt(enterMonitorExpr);
-                    units.insertAfter(enterMonitorStmt, stmt);
+                    units.insertAfter(staticInvocation(enterMonitor), stmt);
                     System.out.println("Enter monitor stmt added..");
                 }
                 
                 public void caseExitMonitorStmt(ExitMonitorStmt stmt){
-                    StaticInvokeExpr exitMonitorExpr = Jimple.v().newStaticInvokeExpr(exitMonitor.makeRef());
-                    Unit exitMonitorStmt = Jimple.v().newInvokeStmt(exitMonitorExpr);
-                    units.insertAfter(exitMonitorStmt, stmt);
+                    units.insertAfter(staticInvocation(exitMonitor), stmt);
                     System.out.println("Exit monitor stmt added..");
                 }
             });
-            
         }
-
     }
 
-    private static Local addTmpRef(Body body, String name, String className) {
-        Local tmpRef = Jimple.v().newLocal(name, RefType.v(className));
-        body.getLocals().add(tmpRef);
-        return tmpRef;
+    static Local createLocal(Body body, String name, String type) {
+        Local l = Jimple.v().newLocal(name, RefType.v(type));
+        body.getLocals().add(l);
+        return l;
+    }
+
+    static InvokeStmt staticInvocation(SootMethod m) {
+        return Jimple.v().newInvokeStmt(Jimple.v().newStaticInvokeExpr(m.makeRef()));
+    }
+
+    static InvokeStmt staticInvocation(SootMethod m, Local arg) {
+        return Jimple.v().newInvokeStmt(Jimple.v().newStaticInvokeExpr(m.makeRef(),arg));
     }
 
 }
