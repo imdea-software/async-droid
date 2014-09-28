1c04566de09f3f642887978216492f9a7cec4ffa
Burcu Ozkan <burcukulahcioglu@gmail.com>
2014-09-28 19:51:43 +0300

    Updated to new version after move

--- src/ase/AseTestBridge.java
+++ src/ase/AseTestBridge.java
@@ -1,68 +1,91 @@
-package myScheduler;
+package ase;
 
-import android.content.Context;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import ase.scheduler.NopScheduler;
+import ase.scheduler.RepeatingScheduler;
+import ase.scheduler.Scheduler;
 
 /*
  *  static interface class between the scheduler and the app under test
  */
-public class MyScheduler {
+public class AseTestBridge {
 
-    private static SchedulerRunnable sch;
-    private static DelayServiceConHandler delayCon;
-
+    private static Scheduler scheduler;
+     
     /*
      *  called by UI thread with the application context
      */
-    public static void initiateScheduler(Context context) 
+    public static void initiateScheduler(Activity act) 
     {
-        if (delayCon == null) {
-            delayCon = new DelayServiceConHandler(context); // for IPC with DelayService
-            sch = new SchedulerRunnable(delayCon); // to perform scheduling
-            Thread t = new Thread(sch);
-            t.setName("MySchedulerThread");
-            t.start();
-            sch.wakeScheduler();
+        setTestParameters(act);
+        runSchedulerThread();
+    }
+    /*
+     *  set the number of delays and inputs
+     */
+    private static void setTestParameters(Activity act){
+        int numDelays=0, numInputs=0;
+        String mode = null;
+        Intent intent = act.getIntent();
+        Bundle bundle = intent.getExtras();
+        if (bundle != null) {
+            mode = bundle.getString("mode");
+            numDelays = bundle.getInt("numDelays");
+            numInputs = bundle.getInt("numInputs");
+        }   
+        Log.i("MyScheduler", String.format("Parameters: numDelays: %d numInputs: %d mode: %s", numDelays, numInputs, mode));
+        if(mode != null && mode.equalsIgnoreCase("record")){
+            scheduler = new NopScheduler(act);
+
+        }else{    
+            scheduler = new RepeatingScheduler(numDelays, numInputs, act.getApplicationContext(), act.getWindow().getDecorView().getRootView()); 
         }
+    }
+    
+    public static void runSchedulerThread(){
+        scheduler.runScheduler();
     }
 
     /*
      *  application thread waits for its signal to start/resume
      */
     public static void waitMyTurn() {    
-        sch.sendThreadInfo(); // add thread to the scheduling list
-        sch.waitMyTurn(Thread.currentThread().getId());
+        scheduler.waitMyTurn();
     }
     
     /*
      *  application thread yields
      */
     public static void yield() {
-        sch.yield(Thread.currentThread().getId());
+        scheduler.yield();
     }
         
     /*
      *  application thread notify scheduler when they are completed
      */
     public static void notifyScheduler() {            
-        sch.notifyScheduler();
+        scheduler.notifyScheduler();
     }
     
     /*
      *  application thread enters in a monitor
      */
     public void enterMonitor(){
-        sch.enterMonitor();
+        scheduler.enterMonitor();
     }
     
     /*
      *  application thread exits a monitor
      */
     public void exitMonitor(){
-        sch.exitMonitor();
+        scheduler.exitMonitor();
     }
     
-    
-
 }
+
+
 
 
