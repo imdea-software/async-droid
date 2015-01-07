73e9287348aa803f834cbddc73bbc0fe4929264b
Burcu Kulahcioglu Ozkan <burcukulahcioglu@gmail.com>
2015-01-07 20:24:50 +0200

    Input injection by messages instead of runnables

--- src/ase_scheduler/src/ase/repeater/InputRepeater.java
+++ src/ase_scheduler/src/ase/repeater/InputRepeater.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import android.os.Handler;
 import android.os.Looper;
+import android.os.Message;
 import android.util.Log;
 import ase.*;
 import ase.recorder.ViewTraverser;
@@ -14,7 +15,6 @@ public class InputRepeater implements Runnable {
     // its thread data is created and added into scheduler list when it
     // registers (sends its info) and waits for its turn
     private List<AseEvent> eventList;
-    private Handler handlerToUI;
     
     // to keep track of the status of InputRepeater thread's job
     // counts the number of posted event invoker runnables to UI
@@ -24,14 +24,12 @@ public class InputRepeater implements Runnable {
     // incremented when posted task to UI could invoke (view exists in current layout) the event
     // ensures the correct order of invocations when view is not in the layout
     private int inputsDispatched = 0;
-    private int trials = 0;
     
     public InputRepeater(List<AseEvent> events) {
         eventList = events;
-        handlerToUI = new Handler(Looper.getMainLooper());
         inputsToGo=eventList.size();
     }
-
+    
     @Override
     public void run() {
         Log.i("Repeater", "In thread: " + Thread.currentThread().getName()
@@ -60,36 +58,43 @@ public class InputRepeater implements Runnable {
         Log.i("Repeater", "Completed posting inputs.");
     }
     
-    // for now, only clicks
     public void sendEventToApp() {
-        AseTestBridge.incNumUIBlocks();  // posts event (runnable to click) into UI thread
-        handlerToUI.post(new Runnable() {
-            public void run() {
-                AseEvent event = eventList.get(inputsDispatched);
-                AseTestBridge.waitForDispatch();
-
-                if(!event.isFirable()) {
-                    if (trials > 10) return;
-                    handlerToUI.post(this);
-                    trials ++;
-                    Log.i("Repeater", "Sending again " + Integer.toHexString(event.viewId));
-                    AseTestBridge.notifyDispatcher();  
-                    return;
-                }
-
-                // counter provides invoking the events in order
-                incrementInputsDispatched();
-                trials = 0;
-
-                // call event's listener
-                event.injectEvent();
-
-                AseTestBridge.decNumUIBlocks(); // runnable to click consumed
-                AseTestBridge.notifyDispatcher();
-            }
-        });
+        AseTestBridge.incNumUIBlocks();  // posts event (message to click) into UI thread
+        
+        // Message arg1 is the number of trials to inject the event
+        Message m = handlerForEventInjection.obtainMessage(1, 0);
+        handlerForEventInjection.sendMessage(m);
     }
 
+    private Handler handlerForEventInjection = new Handler(Looper.getMainLooper()) {
+        @Override
+        public void handleMessage(Message message) {
+            AseEvent event = eventList.get(inputsDispatched);
+            AseTestBridge.waitForDispatch();
+
+            if(!event.isFirable()) {
+                if (message.arg1 > 10) return;
+                Message m = handlerForEventInjection.obtainMessage(1, message.arg1+1); // increment trials
+                this.sendMessage(m);
+                Log.i("Repeater", "Sending again " + Integer.toHexString(event.viewId));
+                AseTestBridge.notifyDispatcher();  
+                return;
+            }
+
+            // counter provides invoking the events in order
+            incrementInputsDispatched();
+            // call event's listener
+            event.injectEvent();
+
+            AseTestBridge.decNumUIBlocks(); // runnable to click consumed
+            AseTestBridge.notifyDispatcher();
+        }
+        
+        public String toString(){
+            return "Ase Handler for Event Injection";
+        }
+    };
+    
     public void incrementInputsDispatched(){
         inputsDispatched ++;
     }
