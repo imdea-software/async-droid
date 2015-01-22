7de1316d005ac9edfe3e1c4efbcb053f95ffb9b2
Burcu Kulahcioglu Ozkan <burcukulahcioglu@gmail.com>
2015-01-22 10:18:44 +0200

    Reading classes from different libraries and reorganization (android.app, android.support.v4.app, android.support.v7.app)

--- src/ase_scheduler/src/ase/util/ReflectionUtils.java
+++ src/ase_scheduler/src/ase/util/ReflectionUtils.java
@@ -13,6 +13,19 @@ import android.widget.AdapterView;
 
 public class ReflectionUtils {
 
+    private static final String CN_ACTIVITY = "android.app.Activity"; 
+    private static final String CN_ACTIVITY_SUPPORT = "android.support.v4.app.FragmentActivity"; 
+    private static final String CN_FRAGMENT_MANAGER = "android.app.FragmentManagerImpl";
+    private static final String CN_FRAGMENT_MANAGER_SUPPORT = "android.support.v4.app.FragmentManagerImpl";
+    private static final String CN_FRAGMENT = "android.app.Fragment";
+    private static final String CN_FRAGMENT_SUPPORT = "android.support.v4.app.Fragment";
+    private static final String CN_ACTION_BAR_TAB = "android.app.ActionBar$Tab";
+    private static final String CN_ACTION_BAR_TAB_SUPPORT = "android.support.v7.app.ActionBar$Tab";
+    private static final String CN_ASYNC_TASK = "android.os.AsyncTask";
+    private static final String CN_ASYNC_TASK_SERIAL_EXECUTOR = "android.os.AsyncTask$SerialExecutor";
+    private static final String CN_VIEW = "android.view.View";
+    private static final String CN_VIEW_LISTENER_INFO = "android.view.View$ListenerInfo";
+
     public static OnClickListener getOnClickListener(View view) {
         OnClickListener listener = null;
         try {
@@ -26,7 +39,7 @@ public class ReflectionUtils {
 
             // get the field mOnClickListener
             Field listenerField = null;
-            listenerField = Class.forName("android.view.View$ListenerInfo")
+            listenerField = Class.forName(CN_VIEW_LISTENER_INFO)
                     .getDeclaredField("mOnClickListener");
             if (listenerField != null && myLiObject != null) {
                 listener = (View.OnClickListener) listenerField.get(myLiObject);
@@ -68,11 +81,11 @@ public class ReflectionUtils {
     public static ArrayDeque<Runnable> getAsyncTaskSerialExecutorTasks() {
         try {
             // get the serial executor instance
-            Field serialExecutorField = Class.forName("android.os.AsyncTask").getDeclaredField("SERIAL_EXECUTOR");
+            Field serialExecutorField = Class.forName(CN_ASYNC_TASK).getDeclaredField("SERIAL_EXECUTOR");
             Object serialExecutor = serialExecutorField.get(null);
 
             // get mTasks field
-            Class serialExecutorClass = Class.forName("android.os.AsyncTask$SerialExecutor");
+            Class serialExecutorClass = Class.forName(CN_ASYNC_TASK_SERIAL_EXECUTOR);
             Field mTasksField = serialExecutorClass.getDeclaredField("mTasks");
             mTasksField.setAccessible(true);
             Object mTasks = mTasksField.get(serialExecutor);
@@ -88,11 +101,11 @@ public class ReflectionUtils {
     public static boolean isAsyncTaskSerialThreadActive() {
         try {
             // get the serial executor instance
-            Field serialExecutorField = Class.forName("android.os.AsyncTask").getDeclaredField("SERIAL_EXECUTOR");
+            Field serialExecutorField = Class.forName(CN_ASYNC_TASK).getDeclaredField("SERIAL_EXECUTOR");
             Object serialExecutor = serialExecutorField.get(null);
 
             // get mTasks field
-            Class serialExecutorClass = Class.forName("android.os.AsyncTask$SerialExecutor");
+            Class serialExecutorClass = Class.forName(CN_ASYNC_TASK_SERIAL_EXECUTOR);
             Field mActiveField = serialExecutorClass.getDeclaredField("mActive");
             mActiveField.setAccessible(true);
             Object mActive = mActiveField.get(serialExecutor);
@@ -103,48 +116,44 @@ public class ReflectionUtils {
         return false;
     }
 
+    /**
+     * Returns the list of active fragments in the activity
+     * If the act is of type android.app.Activity 
+     *   - Gets the fragments from: android.app.FragmentManagerImpl 
+     * If the act is of type android.support.v4.app.FragmentActivity
+     *   - Gets the fragments from: android.support.v4.app.FragmentManagerImpl
+     * (FragmentManager is defined in Android API level 11)
+     */ 
     public static List<Object> getFragments(Activity act) {
-        try
-        {
-            Class supFrag = Class.forName ("android.support.v4.app.FragmentActivity");
-            Log.v("Reflection", "Get fragments from: android.support.v4.app.FragmentActivity");
-            // reads fragments from android.support.v4.app.FragmentManager in android.support.v4.app.FragmentActivity
-            // parameter is of type android.support.v4.app.FragmentActivity or android.app.Activity (if it does not have fragments)     
-            return getFragmentsFromClass(act, "android.support.v4.app.FragmentActivity", "android.support.v4.app.FragmentManagerImpl");
+        String activityClassName = null, 
+               fragmentManagerClassName = null, 
+               getFMMethodName = null;
+        
+        if(getSuperClassOfType(act.getClass(), CN_ACTIVITY_SUPPORT) != null) {
+            activityClassName = CN_ACTIVITY_SUPPORT;
+            fragmentManagerClassName = CN_FRAGMENT_MANAGER_SUPPORT;
+            getFMMethodName = "getSupportFragmentManager";
+        } else if(getSuperClassOfType(act.getClass(), CN_ACTIVITY) != null) {
+            activityClassName = CN_ACTIVITY;
+            fragmentManagerClassName = CN_FRAGMENT_MANAGER;
+            getFMMethodName = "getFragmentManager";
         }
-        catch (ClassNotFoundException e)
-        {
-            try {
-                Class frag = Class.forName ("android.app.Fragment");
-                Log.v("Reflection", "Get fragments from: android.app.Fragment");
-                // reads fragments from android.app.FragmentManager
-                // parameter is of type android.app.Activity                
-                return getFragmentsFromClass(act, "android.app.Activity", "android.app.FragmentManagerImpl");                  
-            } catch (ClassNotFoundException e1) {
-                Log.e("Reflection", "Can not read fragment info");
-            }  
-        }
-        return null;
-    }
 
-    @SuppressWarnings("unchecked")
-    private static List<Object> getFragmentsFromClass(Activity act, String actClass, String fragManClass) {
         List<Object> activeFragments = null;
         try {
-            // get fragment manager by activity.getSupportFragmentManager()
             Method getFragmentManagerMethod = null;
-            Class fragmentActivity = getSuperClassOfType(act.getClass(), actClass);
-            getFragmentManagerMethod = fragmentActivity.getDeclaredMethod("getSupportFragmentManager");
+            Class activity = getSuperClassOfType(act.getClass(), activityClassName);
+            getFragmentManagerMethod = activity.getDeclaredMethod(getFMMethodName);
             Object fragmentManager = getFragmentManagerMethod.invoke(act);
 
             Field mActiveField = null;
-            Class<?> innerClass = Class.forName(fragManClass);
-            mActiveField = innerClass.getDeclaredField("mActive");
+            Class<?> fragmentManClass = Class.forName(fragmentManagerClassName);
+            mActiveField = fragmentManClass.getDeclaredField("mActive");
             mActiveField.setAccessible(true);
             activeFragments = (List<Object>) mActiveField.get(fragmentManager);
         } catch (Exception ex) {
             activeFragments = null;
-            Log.e("Reflection", "Can not read active fragments");
+            Log.w("Reflection", "Can not read active fragments: fmMethod" + getFMMethodName, ex);
         }
 
         return activeFragments;
@@ -152,38 +161,25 @@ public class ReflectionUtils {
     
     /**
      * Returns the name of the fragment in which the view with viewID is inflated
+     * If the act is of type android.app.Activity 
+     *   - Gets the view of the fragment in: android.app.Fragment 
+     * If the act is of type android.support.v4.app.FragmentActivity
+     *   - Gets the view of the fragment in: android.support.v4.app.Fragment 
+     * (Fragment is defined in Android API level 11)
      */
-    @SuppressWarnings({ "unchecked", "unused" })
+    @SuppressWarnings({ "unchecked", "rawtypes" })
     public static String getFragmentByViewID(Activity act, int viewId) {
-        try
-        {
-            Class supFrag = Class.forName ("android.support.v4.app.FragmentActivity");
-            Log.v("Reflection", "Get fragments from: android.support.v4.app.FragmentActivity");
-            // reads fragments from android.support.v4.app.FragmentManager in android.support.v4.app.FragmentActivity
-            // parameter is of type android.support.v4.app.FragmentActivity or android.app.Activity (if it does not have fragments)     
-            return getFragmentByViewIdFromClass(act, viewId, "android.support.v4.app.Fragment");
+        String fragmentClassName = null;
+        if(getSuperClassOfType(act.getClass(), CN_ACTIVITY_SUPPORT) != null) {
+            fragmentClassName = CN_FRAGMENT_SUPPORT;
+        } else if(getSuperClassOfType(act.getClass(), CN_ACTIVITY) != null) {
+            fragmentClassName = CN_FRAGMENT;
         }
-        catch (ClassNotFoundException e)
-        {
-            try {
-                Class frag = Class.forName ("android.app.Fragment");
-                Log.v("Reflection", "Get fragments from: android.app.Fragment");
-                // reads fragments from android.app.FragmentManager
-                // parameter is of type android.app.Activity                
-                return getFragmentByViewIdFromClass(act, viewId, "android.app.Fragment");                  
-            } catch (ClassNotFoundException e1) {
-                Log.e("Reflection", "Can not read fragment info");
-            }  
-        }
-        return null;
-    }
-    
-    @SuppressWarnings("unchecked")
-    private static String getFragmentByViewIdFromClass(Activity act, int viewId, String fragClassName) {
+
         try {
             Method isVisibleMethod = null, getViewMethod = null, findViewByIdMethod = null;
-            Class fragmentClass = Class.forName(fragClassName);
-            Class viewClass = Class.forName("android.view.View");
+            Class fragmentClass = Class.forName(fragmentClassName);
+            Class viewClass = Class.forName(CN_VIEW);
 
             isVisibleMethod = fragmentClass.getDeclaredMethod("isVisible");
             getViewMethod = fragmentClass.getDeclaredMethod("getView");
@@ -191,51 +187,40 @@ public class ReflectionUtils {
 
             for(Object f: getFragments(act)) {
                 Object rootView = getViewMethod.invoke(f);
-
                 View v = (View) findViewByIdMethod.invoke(rootView, viewId);
                 Boolean b = (Boolean) isVisibleMethod.invoke(f);
                 if( (v != null) && (b.booleanValue())) {
                     return f.getClass().getName();
                 }
             }
-
         } catch (Exception ex) {
-            Log.e("Reflection", "Can not read active fragments");
+            Log.e("Reflection", "Can not read active fragments ", ex);
         }
 
         return null;
     }
     
+    /**
+     * Checks whether the tab is android.app.ActionBar.Tab or android.support.v7.app.ActionBar.Tab
+     * Returns the position of the tab in ActionBar
+     * (ActionBar is defined in Android API level 11)
+     */
     public static int getActionBarTabPosition(Object tab) {
-        String actionBarTabClassName;
-        try
-        {
-            Class supTabClass = Class.forName("android.support.v7.app.ActionBar$Tab");
-            actionBarTabClassName = "android.support.v7.app.ActionBar$Tab";
+        String actionBarTabClassName = null;
+        if(getSuperClassOfType(tab.getClass(), CN_ACTION_BAR_TAB) != null) {
+            actionBarTabClassName = CN_ACTION_BAR_TAB;
+        } else if(getSuperClassOfType(tab.getClass(), CN_ACTION_BAR_TAB_SUPPORT) != null) {
+            actionBarTabClassName = CN_ACTION_BAR_TAB_SUPPORT;
         }
-        catch (ClassNotFoundException e)
-        {
-            try {
-                Class tabClass = Class.forName("android.app.ActionBar$Tab");      
-                actionBarTabClassName = "android.app.ActionBar$Tab";
-            } catch (ClassNotFoundException e1) {
-                Log.e("Reflection", "Can not read tab position");
-                return -1;
-            }  
-        }
-        Log.v("Reflection", "Get fragments from: " + actionBarTabClassName);
-        return getActionBarTabPositionFromClass(tab, actionBarTabClassName);
-    }
-    
-    private static int getActionBarTabPositionFromClass(Object tab, String className) {
+
         try {
-            Class actionBarTab = Class.forName(className);
+            Class actionBarTab = Class.forName(actionBarTabClassName);
             Method getPositionMethod = actionBarTab.getDeclaredMethod("getPosition");
             Integer position = (Integer) getPositionMethod.invoke(tab);
             return position.intValue();
             
         } catch (Exception ex) {
-            Log.e("Reflection", "Can not read tab position using " + className);
+            Log.e("Reflection", "Can not read tab position using " + actionBarTabClassName);
         }      
         return -1;
     }
@@ -243,8 +228,9 @@ public class ReflectionUtils {
     @SuppressWarnings("rawtypes")
     public static Class getSuperClassOfType(Class clazz, String superClassName) {
         Class tempClass = clazz;
-        while (tempClass != null && !tempClass.getName().equals(superClassName))
+        while (tempClass != null && !tempClass.getName().equals(superClassName)) {
             tempClass = tempClass.getSuperclass();
+        }
 
         return tempClass;
     }
